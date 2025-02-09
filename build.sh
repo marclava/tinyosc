a0df628350799339b62730d31b89f4f145b70d05
Marc Lavallée <50078614+marclava@users.noreply.github.com>
2025-02-08 19:03:09 -0500

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if type "clang" > /dev/null 2>&1; then
-  clang *.c -Werror -O0 -g -o tinyosc
-else
-  gcc *.c -Werror -std=c99 -O0 -g -o tinyosc
-fi
+# catch all warning options :
+WARNINGS="-Wall -Wextra -Wfatal-errors"
+
+# for degugging (add native debug info):
+# CFLAGS="-g"
+# for production (optimize and strip debug info):
+CFLAGS="-O -s"
+
+# set C compiler, clang is prefered :
+[ $(which "clang") ] && cc=clang || cc=gcc
+
+# compilation of test program :
+cc *.c ${WARNINGS} ${CFLAGS} -o tinyosc
